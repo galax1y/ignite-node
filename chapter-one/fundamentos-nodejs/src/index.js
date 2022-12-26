d74553d5167b2eb5ad5df387e96b9d5facda15a3
Galax1y-LEOPC <lw1707@hotmail.com>
2022-12-26 11:03:38 -0300

    Finalizando módulo básico

--- chapter-one/fundamentos-nodejs/src/index.js
+++ chapter-one/fundamentos-nodejs/src/index.js
@@ -2,10 +2,51 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000);
 
+var users = [
+  {
+    id: "0",
+    name: "galaxY",
+  },
+  {
+    id: "1",
+    name: "Anna",
+  },
+];
+
 app.get("/", (request, response) => {
-  response.json({
-    name: "Lucas",
+  response.json(users);
+});
+
+app.post("/users", (request, response) => {
+  users.push(request.body);
+  response.send("User added.");
+});
+
+app.put(`/users/:id`, (request, response) => {
+  let userId = request.params.id;
+
+  users = users.map((user) => {
+    if (user.id === userId) return request.body;
+    else return user;
   });
+  response.send(
+    `Put recebido! substuituindo usuario de id ${userId} por ${JSON.stringify(
+      request.body
+    )}.`
+  );
+});
+
+app.patch("/users/:id", (request, response) => {
+  response.send("Patch recebido!");
+});
+
+app.delete(`/users/:id`, function (request, response) {
+  let userIdToDelete = request.params.id;
+
+  users = users.filter((user) => user.id !== userIdToDelete);
+  response.send("Usuário deletado");
 });
