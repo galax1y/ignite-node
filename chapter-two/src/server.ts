6a8ad9afe70c58d55a66a181202555e2f645d8d2
Lucas Wermann <lw1707@hotmail.com>
2023-03-01 22:45:34 -0300

    Add: Rotas separadas em um arquivo específico

--- chapter-two/src/server.ts
+++ chapter-two/src/server.ts
@@ -1,13 +1,11 @@
 import express from 'express'
-import { categoryRoutes } from './routes/categories.routes'
-import { specificationRoutes } from './routes/specification.routes'
+import { router } from './routes'
 
-const app = express()
+export const app = express()
 
 app.use(express.json())
 
-// Todos os paths dentro de categoryRoutes vão começar com /categories
-app.use('/categories', categoryRoutes)
-app.use('/specifications', specificationRoutes)
+// O import 'router' vai conter todas as rotas da aplicação
+app.use(router)
 
 app.listen(3333, () => console.log('Server listening on port 3333'))
