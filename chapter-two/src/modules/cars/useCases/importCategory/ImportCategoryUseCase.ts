da1fbd92f9ba46cd5359672fc080fb0986b8de6c
Lucas Wermann <lw1707@hotmail.com>
2023-03-02 00:29:39 -0300

    Add: Leitura do CSV de forma assíncrona

--- chapter-two/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ chapter-two/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,18 +1,45 @@
 import fs from 'node:fs'
 import { parse as csvParse } from 'csv-parse'
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository'
+
+interface IImportCategory {
+  name: string
+  description: string
+}
 
 class ImportCategoryUseCase {
-  execute(file: Express.Multer.File): void {
-    const stream = fs.createReadStream(file.path)
+  constructor(private categoriesRepository: ICategoriesRepository) {}
 
-    const parseFile = csvParse()
-
-    // Função pipe coleta o chunk de dentro do Stream e o envia para o parser
-    stream.pipe(parseFile)
-
-    parseFile.on('data', async (line) => {
-      console.log(line)      
+  private loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path)
+      const categories: IImportCategory[] = []
+  
+      const parseFile = csvParse()
+  
+      // Função pipe coleta o chunk de dentro do Stream e o envia para o parser
+      stream.pipe(parseFile)
+  
+      // Aqui, o csv-parser lê cada chunk (linha) chegando através do pipe
+      parseFile.on('data', async (line) => {
+        const [name, description] = line
+        categories.push({
+          name,
+          description,
+        })
+      })
+      .on('end', () => {
+        resolve(categories)
+      })
+      .on('error', (err) => {
+        reject(err)
+      })
     })
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file)
+    console.log(categories)
   }
 }
 
