e8aded995bc4f9661d6a68ded1f1c8c7243f19c5
Lucas Wermann <lw1707@hotmail.com>
2023-03-03 10:15:17 -0300

    Bump: docker-compose & typeorm

--- chapter-three/src/database/index.ts
+++ chapter-three/src/database/index.ts
@@ -1,3 +1,19 @@
-import { createConnection } from 'typeorm'
+import { DataSource } from 'typeorm';
 
-createConnection()
+const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'docker',
+  password: 'ignite',
+  database: 'rentx', // o nome correto do database é 'rentx'
+});
+
+// Deve exportar essa função
+export function createConnection(
+  host = 'database_ignite',
+): Promise<DataSource> {
+  return AppDataSource.setOptions({ host }).initialize();
+}
+
+export default AppDataSource;
