0229a6cc21c24957a50786944d7c97ccf72d479a
Lucas Wermann <lw1707@hotmail.com>
2023-03-09 14:17:51 -0300

    Add: Middleware de autenticação + Erro custom

---
+++ chapter-three/src/errors/AppError.ts
@@ -0,0 +1,13 @@
+// Custom Error object
+class AppError {
+  public readonly message: string
+  public readonly statusCode: number
+
+  // Default status code is 400
+  constructor(message: string, statusCode = 400) {
+    this.message = message
+    this.statusCode = statusCode
+  }
+}
+
+export { AppError }
---
+++ chapter-three/src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from 'express';
+import { UsersRepository } from '../modules/accounts/repositories/implementations/UsersRepository';
+import { AppError } from '../errors/AppError';
+import { verify } from 'jsonwebtoken'
+
+interface IPayload {
+  sub: string
+
+}
+
+export async function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
+  const authHeader = request.headers.authorization
+
+  if (!authHeader) {
+    throw new AppError('Auth token missing', 401)
+  }
+
+  const [, token] = authHeader.split(' ')
+
+  try{
+    const { sub: user_id } = verify(token, "d652eeeea9a382e2b37ad73e0a66b131") as IPayload
+
+    const usersRepository = new UsersRepository()
+
+    const user = await usersRepository.findById(user_id)
+
+    if (!user) {
+      throw new AppError('User does not exist', 401)
+    }
+    next()
+  } catch {
+    throw new AppError('Invalid auth token', 401)
+  }
+}
