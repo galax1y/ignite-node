924f78b1436d24bb0de6df2554bd5be5c4df22d8
Lucas Wermann <lw1707@hotmail.com>
2023-03-09 16:19:37 -0300

    Alter: Entity User agora contém avatar

--- chapter-three/src/modules/accounts/entities/User.ts
+++ chapter-three/src/modules/accounts/entities/User.ts
@@ -2,34 +2,37 @@ import { v4 as uuidv4 } from 'uuid'
 import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm'
 // Faz parte da estrutura do projeto, agindo como uma interface: Define o que é um User
 
-@Entity("users")
+@Entity('users')
 class User {
-  @PrimaryColumn()
-  id: string
+	@PrimaryColumn()
+	id: string
 
-  @Column()
-  name: string
+	@Column()
+	name: string
 
-  @Column()
-  password: string
-  
-  @Column()
-  email: string
+	@Column()
+	password: string
 
-  @Column()
-  driver_license: string
-  
-  @Column()
-  isAdmin: boolean
-  
-  @CreateDateColumn()
-  created_at: Date
+	@Column()
+	email: string
 
-  constructor() {
-    if (!this.id) {
-      this.id = uuidv4()
-    }
-  }
+	@Column()
+	driver_license: string
+
+	@Column()
+	isAdmin: boolean
+
+	@Column()
+	avatar: string
+
+	@CreateDateColumn()
+	created_at: Date
+
+	constructor() {
+		if (!this.id) {
+			this.id = uuidv4()
+		}
+	}
 }
 
 export { User }
